0cf98a46d296f3e4294ae3d93172dee0ea2c27c1
Andie Mkhala <andilemkhala25@gmail.com>
2023-03-21 07:38:52 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
-
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * print_dog - prints a struct dog
- * @d: struct dog to print
+ * print_dog-prints content of struct dog
+ * @d:struct dog variable to be printed
+ * Return:void
  */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	return;
-
-
-	if (d->name == NULL)
-	d->name = "(nil)";
-	if (d->owner == NULL)
-	d->owner = "(nil)";
-
-
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	{
+		return;
 	}
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,35 +1,27 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog- comtains a string for a dogs detail
+ * including name of dog, age and name of owner
+ * @name:name of dog
+ * @age:age of dog
+ * @owner:name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
+/**
+ * dog_t- typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
-
-
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
-
-
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
